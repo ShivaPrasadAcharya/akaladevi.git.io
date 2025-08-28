13c60f7ed913c5353e4e8cf2f7d10ed3eb5f3aac
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-08-28 22:29:22 +0545

    Update data9Videos1.js

--- data9Videos1.js
+++ data9Videos1.js
@@ -1,7 +1,7 @@
 // data9video1.js - Videos Dataset
 var data9Videos1 = `ID,Title,Description,shorturl,longurl
 001,Motivation for Exam,Premanadaji,see video,https://www.youtube.com/shorts/Jr4_hHwVfJw
-002,Temple,Akala Devi,अकलादेव-लामाचौर,,akaladevi2.mp4
+002,Religion+,PodCast,Shankaracharya Nischalananda,https://www.youtube.com/watch?v=G6k8IQ_sYpk&t=186s
 `;
 
 // Dataset metadata
